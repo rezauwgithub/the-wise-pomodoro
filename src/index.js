f5f60fb8d3b954af38b02fe09cfac61c52418c80
Marc Busqué <marc@lamarciana.com>
2018-01-02 12:20:21 +0100

    Hydrate state from/to localStorage

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { loadState, saveState } from './localStorage'
+
 import 'spectre.css/dist/spectre.min.css'
 
-const store = createStore(pomodoroReducer)
+const persistedState = loadState()
+const store = createStore(
+  pomodoroReducer,
+  persistedState 
+)
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 render(
   <Provider store={store}>
