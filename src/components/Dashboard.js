34e4a93e82eb969df67200215112e96b08d56332
Marc Busqué <marc@lamarciana.com>
2018-01-03 19:23:53 +0100

    Responsive design

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,7 @@ function FormElement(props) {
   const {name, label, value, onChange, disabled} = props
 
   return (
-    <div className="form-group column col-1 col-mx-auto">
+    <div className="form-group column col-1 col-sm-8 col-md-2 col-mx-auto">
       <label htmlFor={name} className="form-label text-center">{label}</label>
       <input disabled={disabled} type="number" name={name} value={value} onChange={onChange} className="form-input text-center" />
     </div>
@@ -27,12 +27,12 @@ function Dashboard(props) {
           <FormElement label="Long rest" name="longRest" value={formatInteger(longRest)} onChange={changeParameter} disabled={startDisabled} />
           <FormElement label="Iterations" name="iterations" value={formatInteger(iterations)} onChange={changeParameter} disabled={startDisabled} />
         </div>
-        <div className="columns">
-          <input type="submit" disabled={startDisabled} value="Start" className="btn btn-primary mt-2 column col-2 col-mx-auto"/>
+        <div className="columns mt-2">
+          <input type="submit" disabled={startDisabled} value="Start" className="btn btn-primary column col-2 col-sm-8 col-md-3 col-mx-auto"/>
         </div>
       </form>
       <div className="columns">
-        <button onClick={stop} disabled={stopDisabled} className="btn btn-default mt-2 column col-2 col-mx-auto">Stop</button>
+        <button onClick={stop} disabled={stopDisabled} className="btn btn-default mt-2 column col-2 col-sm-8 col-md-3 col-mx-auto">Stop</button>
       </div>
     </div>
   )
